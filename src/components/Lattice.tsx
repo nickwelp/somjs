4a6edcf433f39741776c20ce887077168a31be08
nickwelp <nickwelp@gmail.com>
2022-10-01 23:18:03 -0700

    adding more training colors adding to readme

--- src/components/Lattice.tsx
+++ src/components/Lattice.tsx
@@ -18,7 +18,7 @@ const Lattice = ({height=0, width=0}) => {
     console.log('updating lattice state', a);
     setLatticeState(a);
   }
-  const trainerDataLength = 9;
+  const trainerDataLength = 18;
   const trainerVectors = new Array(trainerDataLength);
   for(let i = 0; i<trainerDataLength; i ++){
     trainerVectors[i] = new Vector(3);
@@ -60,7 +60,49 @@ const Lattice = ({height=0, width=0}) => {
   trainerVectors[8][1] = 1;
   trainerVectors[8][2] = 0;
   // console.log(trainerVectors);
- 
+   // dark red
+   trainerVectors[9][0] = 0.5;
+   trainerVectors[9][1] = 0;
+   trainerVectors[9][2] = 0;
+   // light red
+   trainerVectors[10][0] = 1;
+   trainerVectors[10][1] = 0.5;
+   trainerVectors[10][2] = 0.5;
+ // dark yellow
+  trainerVectors[11][0] = 0.5;
+  trainerVectors[11][1] = 0.5;
+  trainerVectors[11][2] = 0;
+  // light yellow
+  trainerVectors[12][0] = 1;
+  trainerVectors[12][1] = 1;
+  trainerVectors[12][2] = 0.5;
+    // light yellow
+    trainerVectors[13][0] = 1;
+    trainerVectors[13][1] = 1;
+    trainerVectors[13][2] = 0.5;
+    // light green
+    trainerVectors[13][0] = 0.5;
+    trainerVectors[13][1] = 1;
+    trainerVectors[13][2] = 0.5;
+    // dark green
+    trainerVectors[14][0] = 0;
+    trainerVectors[14][1] = 0.5;
+    trainerVectors[14][2] = 0;
+   // dark violet
+   trainerVectors[15][0] = 0.5;
+   trainerVectors[15][1] = 0;
+   trainerVectors[15][2] = 0.5;
+   // dark blue
+   trainerVectors[16][0] = 0;
+   trainerVectors[16][1] = 0;
+   trainerVectors[16][2] = 0.5;
+   // light blue
+   trainerVectors[17][0] = 0.5;
+   trainerVectors[17][1] = 0.5;
+   trainerVectors[17][2] = 1;
+
+
+
   const [somTrainer, replaceSOMTrainer] = useState(new SOMTrainer(
     latticeState,
     trainerVectors,
