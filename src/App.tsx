915e8f4a05bd5839168b6f7cad15599062703118
nickwelp <nickwelp@gmail.com>
2022-10-03 23:16:54 -0700

    blockquote

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,20 @@ function App() {
         <h1>SOM JS</h1>
         <p>A Kohenen Self Organizing Feature Map is an algorithmic tool for grouping likes together in a two dimensional space.</p>
         <p>In this example the SOM will group 40 or so colors as identified by the swatches below.</p>
-        <p>Special thanks to this C++ project from AI-Junkie:
-    Kohonen's Self Organizing Feature Maps<br />
-    Name:  SOMDemo (part one)<br />
-    Desc:  Code to accompany part one of the SOM tutorial found at  <br />
-           www.ai-junkie.com.<br />
-           This demo creates a Kohonen Self Organizing Feature Map and <br />
-           uses it to classify a varying amount of colors. <br />
-           <br />
-    Author:Mat Buckland 2002 (fup@ai-junkie.com)<br />
-and the JAVA version of that project from <br />
- @author  alanter<br />
-http://www.ai-junkie.com/ann/som/som1.html<br />
-<br />
-        </p>
+        <blockquote><p>Special thanks to this C++ project from AI-Junkie:
+          Kohonen's Self Organizing Feature Maps<br />
+          Name:  SOMDemo (part one)<br />
+          Desc:  Code to accompany part one of the SOM tutorial found at  <br />
+                www.ai-junkie.com.<br />
+                This demo creates a Kohonen Self Organizing Feature Map and <br />
+                uses it to classify a varying amount of colors. <br />
+                <br />
+          Author:Mat Buckland 2002 (fup@ai-junkie.com)<br />
+      and the JAVA version of that project from <br />
+      @author  alanter<br />
+      http://www.ai-junkie.com/ann/som/som1.html<br /></p>
+</blockquote>
+
       </header>
       <AppContext.Provider value={{latticeData, setLatticeData}}>
         <Lattice height={50} width={50} />
